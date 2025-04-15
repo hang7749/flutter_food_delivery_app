3840e0b2f6fb55043f18b7d54c134eb0a35912f1
Yi Hang <yihang19990726@gmail.com>
2025-04-15 12:00:39 +0800

    image assets and onboarding page ui

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/services/widget_support.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView( // 👈 Add this
+        child: Container(
+          margin: const EdgeInsets.only(top: 40),
+          child: Column(
+            children: [
+              Image.asset(
+                "images/onboard.png",
+              ),
+              const SizedBox(height: 20.0), // Add some space between the image and the text
+              Text(
+                "Welcome to Food Delivery App",
+                textAlign: TextAlign.center,
+                style: AppWidget.HeadlineTextFieldStyle()
+              ),
+              const SizedBox(height: 20.0), // Add some space between the title and the subtitle
+              Text("Come order Now and deliver your food to your home",
+                textAlign: TextAlign.center,
+                style: AppWidget.SimpleTextFieldStyle()
+              ),
+              const SizedBox(height: 20.0), // Add some space between the subtitle and the button
+              Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    "Get Started",
+                    style: AppWidget.SimpleTextFieldStyle().copyWith(color: Colors.white)
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/widget_support.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppWidget {
+  static TextStyle HeadlineTextFieldStyle() {
+    return const TextStyle(
+      fontSize: 30.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    );
+  }
+
+  static TextStyle SimpleTextFieldStyle() {
+    return const TextStyle(
+      fontSize: 18.0,
+      color: Colors.black,
+    );
+  }
+ 
+
+}
